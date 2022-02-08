493a1e57d7cc840b287082ea01064500fe952e60
albinSamefors <81687105+albinSamefors@users.noreply.github.com>
2022-02-08 11:09:13 +0100

    Made menu visible

--- LetsEat/app/src/main/java/com/example/letseat/MainActivity.kt
+++ LetsEat/app/src/main/java/com/example/letseat/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.letseat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.navigation_bar,menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
