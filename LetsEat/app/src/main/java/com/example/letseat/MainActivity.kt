0105bd72b2b7120f18116d67563f53a8901463fd
albinSamefors <81687105+albinSamefors@users.noreply.github.com>
2022-02-10 14:44:03 +0100

    Update Swiper
    Have been working on getting the swipe listener to work but it is does not

--- LetsEat/app/src/main/java/com/example/letseat/MainActivity.kt
+++ LetsEat/app/src/main/java/com/example/letseat/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
 import android.view.GestureDetector
+import androidx.constraintlayout.widget.ConstraintLayout
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,11 +16,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val rightIntent = Intent(this, MapsActivity::class.java)
+        val container = findViewById<ConstraintLayout>(R.id.mainContainer)
+        var swipeListener : SwipeListener = SwipeListener()
+        swipeListener.SwipeListener(container,rightIntent,true,baseContext)
 
 
 
     }
+
 
 
 
--- LetsEat/app/src/main/java/com/example/letseat/SwipeListener.kt
+++ LetsEat/app/src/main/java/com/example/letseat/SwipeListener.kt
@@ -1,89 +1,167 @@
 package com.example.letseat
 
-import androidx.core.content.ContextCompat.startActivity
 import android.content.Intent
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import androidx.core.content.ContextCompat.startActivity
 
 import java.lang.Exception
+import kotlin.coroutines.coroutineContext
 
- class SwipeListener : View.OnTouchListener {
+class SwipeListener() : View.OnTouchListener  {
+
      lateinit var gestureDetector: GestureDetector
 
 
+     fun SwipeListener(view: View ,leftSwipeIntent : Intent, rightSwipeIntent : Intent, context: Context)
+     {
 
-     fun SwipeListener(view: View ,leftSwipeIntent : Intent, rightSwipeIntent : Intent )
-    {
+         val threshold = 100
+         val velocity_threshold = 100
+         val listener = object: GestureDetector.SimpleOnGestureListener(){
+             override fun onDown(e: MotionEvent?): Boolean {
+                 return true
+             }
 
-        val threshold = 100
-        val velocity_threshold = 100
-        val listener = object: GestureDetector.SimpleOnGestureListener(){
-            override fun onDown(e: MotionEvent?): Boolean {
-                return true
-            }
+             override fun onFling(
+                 e1: MotionEvent?,
+                 e2: MotionEvent?,
+                 velocityX: Float,
+                 velocityY: Float
+             ): Boolean {
+                 //Get x and y difference
+                 val xDiff = (e2!!.x - e1!!.x)
+                 val yDiff = (e2.y - e1.y)
+                 try {
+                     //When x is greater then y
+                     if(Math.abs(xDiff) > Math.abs(yDiff))
+                     {
+                         //Checking conditions
+                         if(Math.abs(xDiff) > threshold && Math.abs(velocityX) > velocity_threshold )
+                         {
+                             //When x difference is greater then threshold
+                             //When x velocity is greater then threshold
+                             if(xDiff > 0)
+                             {
+                                 startActivity(context,rightSwipeIntent,null)
+                                 //rightSwipe(rightSwipeIntent)
+                             }
+                             else
+                             {
+                                 startActivity(context,leftSwipeIntent,null)
+                                 //leftSwipe(leftSwipeIntent)
+                             }
+                             return true
+                         }
+                     }
+                     else{
+                         //When y is greater than x
+                         if(Math.abs(yDiff) > threshold && Math.abs(velocityY) > velocity_threshold)
+                         {
+                             //When y difference is greater than threshold
+                             //When y velocity is greater than velocity threshold
+                             if(yDiff > 0)
+                             {
+                                 //When swiped down
 
-            override fun onFling(
-                e1: MotionEvent?,
-                e2: MotionEvent?,
-                velocityX: Float,
-                velocityY: Float
-            ): Boolean {
-                //Get x and y difference
-                val xDiff = (e2!!.x - e1!!.x)
-                val yDiff = (e2.y - e1.y)
-                try {
-                    //When x is greater then y
-                    if(Math.abs(xDiff) > Math.abs(yDiff))
-                    {
-                        //Checking conditions
-                        if(Math.abs(xDiff) > threshold && Math.abs(velocityX) > velocity_threshold )
-                        {
-                            //When x difference is greater then threshold
-                            //When x velocity is greater then threshold
-                            if(xDiff > 0)
-                            {
-                                rightSwipe(rightSwipeIntent)
-                            }
-                            else
-                            {
-                                leftSwipe(leftSwipeIntent)
-                            }
-                            return true
-                        }
-                    }
-                    else{
-                            //When y is greater than x
-                        if(Math.abs(yDiff) > threshold && Math.abs(velocityY) > velocity_threshold)
-                        {
-                            //When y difference is greater than threshold
-                            //When y velocity is greater than velocity threshold
-                            if(yDiff > 0)
-                            {
-                                //When swiped down
+                             }
+                             else
+                             {
+                                 //When swiped up
+                             }
+                             return true
+                         }
+                     }
 
-                            }
-                            else
-                            {
-                                //When swiped up
-                            }
-                            return true
-                        }
-                    }
-
-                }catch ( e: Exception){
-                    e.stackTrace
-                }
-                return false
-            }
-        }
+                 }catch ( e: Exception){
+                     e.stackTrace
+                 }
+                 return false
+             }
+         }
          gestureDetector = GestureDetector(listener)
 
-        view.setOnTouchListener(this)
+         view.setOnTouchListener(this)
 
-    }
+     }
+     fun SwipeListener(view: View ,swipeIntent : Intent, isRight : Boolean, context: Context)
+     {
+
+         val threshold = 100
+         val velocity_threshold = 100
+         val listener = object: GestureDetector.SimpleOnGestureListener(){
+             override fun onDown(e: MotionEvent?): Boolean {
+                 return true
+             }
+
+             override fun onFling(
+                 e1: MotionEvent?,
+                 e2: MotionEvent?,
+                 velocityX: Float,
+                 velocityY: Float
+             ): Boolean {
+                 //Get x and y difference
+                 val xDiff = (e2!!.x - e1!!.x)
+                 val yDiff = (e2.y - e1.y)
+                 try {
+                     //When x is greater then y
+                     if(Math.abs(xDiff) > Math.abs(yDiff))
+                     {
+                         //Checking conditions
+                         if(Math.abs(xDiff) > threshold && Math.abs(velocityX) > velocity_threshold )
+                         {
+                             //When x difference is greater then threshold
+                             //When x velocity is greater then threshold
+                             if((xDiff > 0) && isRight)
+                             {
+                                 startActivity(context,swipeIntent,null)
+                                 rightSwipe(swipeIntent)
+                             }
+                             else
+                             {
+                                 startActivity(context,swipeIntent,null)
+                                 leftSwipe(swipeIntent)
+                             }
+                             return true
+                         }
+                     }
+                     else{
+                         //When y is greater than x
+                         if(Math.abs(yDiff) > threshold && Math.abs(velocityY) > velocity_threshold)
+                         {
+                             //When y difference is greater than threshold
+                             //When y velocity is greater than velocity threshold
+                             if(yDiff > 0)
+                             {
+                                 //When swiped down
+
+                             }
+                             else
+                             {
+                                 //When swiped up
+                             }
+                             return true
+                         }
+                     }
+
+                 }catch ( e: Exception){
+                     e.stackTrace
+                 }
+                 return false
+             }
+         }
+         gestureDetector = GestureDetector(listener)
+
+         view.setOnTouchListener(this)
+
+     }
+
      private fun leftSwipe(leftSwipeIntent: Intent): Intent {
-        return leftSwipeIntent
+         return leftSwipeIntent
+
      }
      private fun rightSwipe(rightSwipeIntent: Intent): Intent {
         return rightSwipeIntent
