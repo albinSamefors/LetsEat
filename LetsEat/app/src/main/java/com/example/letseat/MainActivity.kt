6fedd6a076b722564aa55e86a4f2375cf5dc9bf1
felixsundman <81686723+felixsundman@users.noreply.github.com>
2022-02-17 12:19:43 +0100

    Made the imagebutton clickable so the LoginActivity starts

--- LetsEat/app/src/main/java/com/example/letseat/MainActivity.kt
+++ LetsEat/app/src/main/java/com/example/letseat/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.GestureDetector
 import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.letseat.ui.login.LoginActivity
 
 // TODO: Ändra så att alla färger hämtas ifrån temat istället för de hårdkodade färgerna Samt fixa darkmode
 class MainActivity : AppCompatActivity() {
@@ -30,6 +31,11 @@ class MainActivity : AppCompatActivity() {
         var swipeListener : SwipeListener = SwipeListener()
         swipeListener.SwipeListener(container,rightIntent,false,this)
         */
+        val loginButton = findViewById<ImageButton>(R.id.imageButton)
+        loginButton.setOnClickListener{
+            val intent =Intent (this, LoginActivity::class.java)
+            startActivity(intent)
+        }
 
 
         // Adds the restaurants to the main screen
