473f72562d4f3cb85fdab29a9c0cf35798dc45a0
felixsundman <81686723+felixsundman@users.noreply.github.com>
2022-02-18 18:00:47 +0100

    Deleted previous LoginActivity.kt Added RegistrationActivity.kt and implemented it Added new LoginActivity.kt

--- LetsEat/app/src/main/java/com/example/letseat/MainActivity.kt
+++ LetsEat/app/src/main/java/com/example/letseat/MainActivity.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.GestureDetector
 import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
-import com.example.letseat.ui.login.LoginActivity
 
 // TODO: Ändra så att alla färger hämtas ifrån temat istället för de hårdkodade färgerna Samt fixa darkmode
 class MainActivity : AppCompatActivity() {
@@ -22,21 +21,16 @@ class MainActivity : AppCompatActivity() {
         val logInIntent = Intent(this, LoginActivity::class.java)
         val container = findViewById<ListView>(R.id.restaurantView)
         val mapButton = findViewById<ImageButton>(R.id.mapButton)
-        val loginButton = findViewById<ImageButton>(R.id.logInButton)
        mapButton.setOnClickListener{
            startActivity(rightIntent)
        }
-        loginButton.setOnClickListener{
-            startActivity(logInIntent)
-        }
-
 
 
         /*
         var swipeListener : SwipeListener = SwipeListener()
         swipeListener.SwipeListener(container,rightIntent,false,this)
         */
-        val loginButton = findViewById<ImageButton>(R.id.imageButton)
+        val loginButton = findViewById<ImageButton>(R.id.logInButton)
         loginButton.setOnClickListener{
             val intent =Intent (this, LoginActivity::class.java)
             startActivity(intent)
