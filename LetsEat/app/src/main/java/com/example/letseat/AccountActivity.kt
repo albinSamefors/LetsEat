73d72c04b5283a775af2b3420107fbe9be02ddb3
felixsundman <81686723+felixsundman@users.noreply.github.com>
2022-03-18 15:41:37 +0100

    Account page

--- LetsEat/app/src/main/java/com/example/letseat/AccountActivity.kt
+++ LetsEat/app/src/main/java/com/example/letseat/AccountActivity.kt
@@ -16,18 +16,19 @@ class AccountActivity : AppCompatActivity() {
 	private lateinit var firebaseAuth: FirebaseAuth
 	private lateinit var database: FirebaseDatabase
 	private lateinit var ref: DatabaseReference
+
 	// creating variables for our list view.
 	private var listView: ListView? = null
-
+	var restaurantArrayList: ArrayList<String?>? = null
+	val favouriteRepository = RestaurantRepository()
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_account)
 		//initiation
 		firebaseAuth = FirebaseAuth.getInstance()
-		ref = FirebaseDatabase.getInstance("https://let-s-eat-c3632-default-rtdb.europe-west1.firebasedatabase.app/").getReference("users")
-		ref.child(firebaseAuth.uid!!).child("Favorites")
 		usernameTextView = findViewById(R.id.tvUsername)
 		listView = findViewById(R.id.lvAccountView)
+		restaurantArrayList = ArrayList()
 
 
 
@@ -51,6 +52,7 @@ class AccountActivity : AppCompatActivity() {
 			startActivity(Intent(this, MainActivity::class.java))
 			finish()
 		}
+		initializeListView()
 	}
 
 	override fun onStart() {
@@ -77,5 +79,69 @@ class AccountActivity : AppCompatActivity() {
 					finish()
 				}.show()
 		}
+	}
+	private fun initializeListView() {
+		// creating a new array adapter for our list view.
+	//	val adapter = RestaurantListAdapter(this,R.layout.restaurant_item_white, ,user)// ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, restaurantArrayList)
+
+		// below line is used for getting reference
+		// of our Firebase Database.
+		ref = FirebaseDatabase.getInstance("https://let-s-eat-c3632-default-rtdb.europe-west1.firebasedatabase.app/")
+			.getReference("users")
+
+		ref = ref.child(firebaseAuth.uid!!).child("Favorites")
+
+		ref.addValueEventListener( object : ValueEventListener {
+			override fun onDataChange(snapshot: DataSnapshot) {
+				restaurantArrayList!!.add(snapshot.getValue(String::class.java))
+			}
+
+			override fun onCancelled(error: DatabaseError) {
+				TODO("Not yet implemented")
+			}
+		})
+		// in below line we are calling method for add child event
+		// listener to get the child of our database.
+		ref!!.addChildEventListener(object : ChildEventListener {
+			override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+				// this method is called when new child is added to
+				// our data base and after adding new child
+				// we are adding that item inside our array list and
+				// notifying our adapter that the data in adapter is changed.
+				restaurantArrayList!!.add(snapshot.getValue(String::class.java))
+	//			adapter.notifyDataSetChanged()
+			}
+
+			override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+				// this method is called when the new child is added.
+				// when the new child is added to our list we will be
+				// notifying our adapter that data has changed.
+			//	adapter.notifyDataSetChanged()
+			}
+
+			override fun onChildRemoved(snapshot: DataSnapshot) {
+				// below method is called when we remove a child from our database.
+				// inside this method we are removing the child from our array list
+				// by comparing with it's value.
+				// after removing the data we are notifying our adapter that the
+				// data has been changed.
+				restaurantArrayList!!.remove(snapshot.getValue(String::class.java))
+				//adapter.notifyDataSetChanged()
+			}
+
+			override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+				// this method is called when we move our
+				// child in our database.
+				// in our code we are note moving any child.
+			}
+
+			override fun onCancelled(error: DatabaseError) {
+				// this method is called when we get any
+				// error from Firebase with error.
+			}
+		})
+		// below line is used for setting
+		// an adapter to our list view.
+	//	listView!!.adapter = adapter
 	}
 }
