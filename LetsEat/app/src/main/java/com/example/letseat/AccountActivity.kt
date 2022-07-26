ff7a6643eb68c5837661aff12696ebc7c8db0507
albinSamefors <81687105+albinSamefors@users.noreply.github.com>
2022-07-26 13:04:32 +0200

    Fixes around the Account Activity

--- LetsEat/app/src/main/java/com/example/letseat/AccountActivity.kt
+++ LetsEat/app/src/main/java/com/example/letseat/AccountActivity.kt
@@ -18,22 +18,21 @@ class AccountActivity : AppCompatActivity() {
 	private lateinit var firebaseAuth: FirebaseAuth
 	private lateinit var ref: DatabaseReference
 	private lateinit var userLatLng : LatLng
-	private var listView: ListView? = null
+	private lateinit var listView: ListView
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_account)
-		//Initializing Firebase authentication
 		firebaseAuth = FirebaseAuth.getInstance()
 		emailTextView = findViewById(R.id.textViewEmail)
 		listView = findViewById(R.id.lvAccountView)
 		if(intent.hasExtra("userLat") && intent.hasExtra("userLng"))
 		{
-			var extras = intent.extras
-			userLatLng = LatLng(extras!!.getDouble("userLat", 0.0),extras!!.getDouble("userLng",0.0))
+			val extras = intent.extras
+			userLatLng = LatLng(extras!!.getDouble("userLat", 0.0),extras.getDouble("userLng",0.0))
 
 		}
 
-		val listViewButton = findViewById<ImageButton>(R.id.accountListViewButton)
+		val listViewButton = findViewById<ImageButton>(R.id.restaurantListButton)
 		listViewButton.setOnClickListener {
 			val intent = Intent(this, MainActivity::class.java)
 			startActivity(intent)
@@ -45,7 +44,7 @@ class AccountActivity : AppCompatActivity() {
 			startActivity(intent)
 		}
 
-		val logOutButton = findViewById<Button>(R.id.btnLogout)
+		val logOutButton = findViewById<Button>(R.id.logOutButton)
 		logOutButton.setOnClickListener {
 			Firebase.auth.signOut()
 			Toast.makeText(this, R.string.Successfully_signed_out, Toast.LENGTH_SHORT).show()
@@ -54,10 +53,10 @@ class AccountActivity : AppCompatActivity() {
 		}
 		initializeListView()
 		{
-			var adapter = RestaurantListAdapter(this,R.layout.restaurant_item_white,
+			val adapter = RestaurantListAdapter(this,R.layout.restaurant_item_white,
 				favoriteRestaurantRepository.getAllRestaurants(),userLatLng)
-			listView!!.adapter = adapter
-			listView!!.setOnItemClickListener { parent, view, position, id ->
+			listView.adapter = adapter
+			listView.setOnItemClickListener { parent, view, position, id ->
 				val intent = Intent(this, RestaurantActivity::class.java)
 				intent.putExtra("id", favoriteRestaurantRepository.getSpecificRestaurant(position).id)
 				intent.putExtra("isFavorite",true)
@@ -97,30 +96,26 @@ class AccountActivity : AppCompatActivity() {
 		}
 	}
 	private fun initializeListView(callback : () -> Unit)  {
-		// creating a new array adapter for our list view.
+		// creating a new array adapter for the list view.
 		// below line is used for getting reference
 		// of our Firebase Database.
 		Thread(Runnable {
-		ref = FirebaseDatabase.getInstance("https://let-s-eat-c3632-default-rtdb.europe-west1.firebasedatabase.app/")
+		ref = FirebaseDatabase.getInstance(resources.getString(R.string.database_url))
 			.getReference("users").child(firebaseAuth.currentUser!!.uid)
-		var values = ref.child("Favorites")
-		ref.get().addOnSuccessListener {
-			for(child in it.child("Favorites").children)
-			{
-				val restaurantLat = child.child("restaurantLat").value.toString().toDouble()
-				val restaurantLng = child.child("restaurantLng").value.toString().toDouble()
-				val restaurantLatLng  = LatLng(restaurantLat,restaurantLng)
-				var isOpen = child.child("restaurantOpen").value.toString()
-				favoriteRestaurantRepository.addRestaurant(child.child("restaurantName").value.toString(),restaurantLatLng,child.child("restaurantRating").value.toString().toFloat(),child.child("restaurantAddress").value.toString())
-
-
+			ref.get().addOnSuccessListener {
+				for(child in it.child("Favorites").children) {
+					val restaurantLat = child.child("restaurantLat").value.toString().toDouble()
+					val restaurantLng = child.child("restaurantLng").value.toString().toDouble()
+					val restaurantLatLng  = LatLng(restaurantLat,restaurantLng)
+					favoriteRestaurantRepository.addRestaurant(child.child("restaurantName")
+						.value.toString(),restaurantLatLng,child.child("restaurantRating")
+						.value.toString().toFloat(),child.child("restaurantAddress").value.toString())
+				}
+				callback()
 			}
-			callback()
-
-
-		}
+			ref.get().addOnFailureListener {
+				it.printStackTrace()
+			}
 		}).start()
-
 	}
-
 }
--- LetsEat/app/src/main/java/com/example/letseat/DatabaseRepository.kt
+++ LetsEat/app/src/main/java/com/example/letseat/DatabaseRepository.kt
@@ -1,6 +1,7 @@
 package com.example.letseat
 
 import android.content.ContentValues.TAG
+import android.content.res.Resources
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -8,7 +9,7 @@ import com.google.firebase.database.FirebaseDatabase
 
 object DatabaseRepository{
     fun updateUser(user: FirebaseUser?, newAccount: User) {
-        FirebaseDatabase.getInstance("https://let-s-eat-c3632-default-rtdb.europe-west1.firebasedatabase.app/")
+        FirebaseDatabase.getInstance(Resources.getSystem().getString(R.string.database_url))
             .getReference("users")
             .child(FirebaseAuth.getInstance().currentUser?.uid!!).setValue(newAccount)
             .addOnCompleteListener { task ->
